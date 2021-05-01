6db918bf1aa5959e2c6590bf01b147c114e57c80
Joe Auman <brain.recall@gmail.com>
2021-04-30 22:13:48 -0400

    converted to a PlatformIO project, with OTA and MQTT for the LED; removed the LED and soft AP

--- diy-e14/src/E14.ino
+++ diy-e14/src/E14.ino
@@ -28,19 +28,13 @@
 #include "src/OV2640Streamer.h"
 #include "src/CRtspSession.h"
 
-//#define ENABLE_OLED //if want use oled ,turn on thi macro
-//#define SOFTAP_MODE // If you want to run our own softap turn this on
-#define ENABLE_WEBSERVER
-#define ENABLE_RTSPSERVER
+const char *ssid =     "AP";         // Put your SSID here
+const char *password = "12345";      // Put your PASSWORD here
 
-#ifdef ENABLE_OLED
-#include "SSD1306.h"
-#define OLED_ADDRESS 0x3c
-#define I2C_SDA 14
-#define I2C_SCL 13
-SSD1306Wire display(OLED_ADDRESS, I2C_SDA, I2C_SCL, GEOMETRY_128_32);
-bool hasDisplay; // we probe for the device at runtime
-#endif
+//#define ENABLE_WEBSERVER
+#define ENABLE_RTSPSERVER
+#define ENABLE_MQTT
+#define ENABLE_OTA
 
 // Select camera model
 //#define CAMERA_MODEL_WROVER_KIT
@@ -49,9 +43,34 @@ bool hasDisplay; // we probe for the device at runtime
 //#define CAMERA_MODEL_M5STACK_WIDE
 #define CAMERA_MODEL_AI_THINKER
 
+#define CAM_FRAMESIZE FRAMESIZE_VGA
+#define CAM_QUALITY 12
+#define CAM_FRAMERATE 10
+
 #include "camera_pins.h"
 
-OV2640 cam;
+//Arduino OTA Config
+#define OTA_NAME "ESP32CAM"
+#define OTA_PORT 8266 //default
+#define OTA_PASS NULL
+
+//MQTT Config
+#define MQTT_SERVER "homeassistant.localdomain"
+#define MQTT_PORT 1883
+#define MQTT_USER "mqtt"
+#define MQTT_PASS "mqtt"
+#define MQTT_CLIENT OTA_NAME
+#define MQTT_TOPIC "home/esp32cam/led"
+
+#ifdef ENABLE_OTA
+#include <ArduinoOTA.h>
+#endif
+
+#ifdef ENABLE_MQTT
+#include <PubSubClient.h>
+WiFiClient esp_client;
+PubSubClient mqtt_client(esp_client);
+#endif
 
 #ifdef ENABLE_WEBSERVER
 WebServer server(80);
@@ -61,11 +80,55 @@ WebServer server(80);
 WiFiServer rtspServer(8554);
 #endif
 
+OV2640 cam;
+CStreamer *streamer;
+CRtspSession *session;
+WiFiClient rtsp_client;
 
-#ifdef SOFTAP_MODE
-IPAddress apIP = IPAddress(192, 168, 1, 1);
-#else
-#include "wifikeys.h"
+#ifdef ENABLE_MQTT
+void mqtt_client_callback(char* topic, uint8_t* payload, unsigned int length)
+{
+  char payload_string[8];
+  char payload_string_len = length;
+  int intensity = 0;
+
+  //constrain the length of the payload
+  if (payload_string_len >= sizeof(payload_string))
+  {
+    payload_string_len = sizeof(payload_string) - 1;
+  }
+
+  //convert to a C-string, and then convert to a number
+  memset(payload_string, 0, sizeof(payload_string));
+  memcpy(payload_string, payload, payload_string_len);
+  intensity = atoi((char*)payload_string);
+
+  //TODO set LED
+  #if defined(CAMERA_MODEL_AI_THINKER)
+    digitalWrite(4, intensity);
+  #endif
+}
+
+void mqtt_reconnect()
+{
+  if (mqtt_client.connected() == false)
+  {
+    Serial.print("MQTT Connecting...");
+    do
+    {
+      if (mqtt_client.connect(MQTT_CLIENT, MQTT_USER, MQTT_PASS) == true)
+      {
+        Serial.println(" Connected!");
+        mqtt_client.subscribe(MQTT_TOPIC);
+      }
+      else
+      {
+        Serial.print(".");
+        delay(1000);
+      }
+    } while (mqtt_client.connected() == false);
+  }
+}
 #endif
 
 #ifdef ENABLE_WEBSERVER
@@ -122,31 +185,14 @@ void handleNotFound()
 }
 #endif
 
-void lcdMessage(String msg)
-{
-  #ifdef ENABLE_OLED
-    if(hasDisplay) {
-        display.clear();
-        display.drawString(128 / 2, 32 / 2, msg);
-        display.display();
-    }
-  #endif
-}
-
 void setup()
 {
-  #ifdef ENABLE_OLED
-    hasDisplay = display.init();
-    if(hasDisplay) {
-        display.flipScreenVertically();
-        display.setFont(ArialMT_Plain_16);
-        display.setTextAlignment(TEXT_ALIGN_CENTER);
-    }
-  #endif
-    lcdMessage("booting");
-
     Serial.begin(115200);
-    //while (!Serial);            //wait for serial connection. 
+
+    //setup camera LED
+    #if defined(CAMERA_MODEL_AI_THINKER)
+      pinMode(4, OUTPUT);
+    #endif
 
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
@@ -169,8 +215,8 @@ void setup()
     config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
     config.pixel_format = PIXFORMAT_JPEG;
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12; 
+    config.frame_size = CAM_FRAMESIZE;
+    config.jpeg_quality = CAM_QUALITY; 
     config.fb_count = 2;       
   
     #if defined(CAMERA_MODEL_ESP_EYE)
@@ -182,35 +228,6 @@ void setup()
     
     IPAddress ip;
 
-#ifdef SOFTAP_MODE
-    const char *hostname = "devcam";
-   // WiFi.hostname(hostname); // FIXME - find out why undefined
-    lcdMessage("starting softAP");
-    WiFi.mode(WIFI_AP);
-    
-    bool result = WiFi.softAP(hostname, "12345678", 1, 0);
-    delay(2000);
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-    
-    if (!result)
-    {
-        Serial.println("AP Config failed.");
-        return;
-    }
-    else
-    {
-        Serial.println("AP Config Success.");
-        Serial.print("AP MAC: ");
-        Serial.println(WiFi.softAPmacAddress());
-
-        ip = WiFi.softAPIP();
-
-        Serial.print("Stream Link: rtsp://");
-        Serial.print(ip);
-        Serial.println(":8554/mjpeg/1");
-    }
-#else
-    lcdMessage(String("join ") + ssid);
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED)
@@ -225,9 +242,6 @@ void setup()
     Serial.print("Stream Link: rtsp://");
     Serial.print(ip);
     Serial.println(":8554/mjpeg/1");
-#endif
-
-    lcdMessage(ip.toString());
 
 #ifdef ENABLE_WEBSERVER
     server.on("/", HTTP_GET, handle_jpg_stream);
@@ -236,14 +250,25 @@ void setup()
     server.begin();
 #endif
 
+#ifdef ENABLE_MQTT
+    //Setup MQTT
+    mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
+    mqtt_client.setCallback(mqtt_client_callback);
+    mqtt_reconnect();
+#endif
+
+#ifdef ENABLE_OTA
+    //Setup OTA
+    ArduinoOTA.setPort(OTA_PORT);
+    ArduinoOTA.setHostname(OTA_NAME);
+    ArduinoOTA.setPassword(OTA_PASS);
+    ArduinoOTA.begin();
+#endif
+
 #ifdef ENABLE_RTSPSERVER
     rtspServer.begin();
 #endif
 }
-
-CStreamer *streamer;
-CRtspSession *session;
-WiFiClient client; // FIXME, support multiple clients
 
 void loop()
 {
@@ -251,24 +276,32 @@ void loop()
     server.handleClient();
 #endif
 
+#ifdef ENABLE_OTA
+    ArduinoOTA.handle();
+#endif
+
+#ifdef ENABLE_MQTT
+    mqtt_reconnect();
+    mqtt_client.loop();
+#endif
+
 #ifdef ENABLE_RTSPSERVER
-    uint32_t msecPerFrame = 100;
+    uint32_t msecPerFrame = (1000 / CAM_FRAMERATE);
     static uint32_t lastimage = millis();
 
     // If we have an active client connection, just service that until gone
-    // (FIXME - support multiple simultaneous clients)
     if(session) {
         session->handleRequests(0); // we don't use a timeout here,
         // instead we send only if we have new enough frames
 
         uint32_t now = millis();
-        if(now > lastimage + msecPerFrame || now < lastimage) { // handle clock rollover
+        if((now > (lastimage + msecPerFrame)) || (now < lastimage)) { // handle clock rollover
             session->broadcastCurrentFrame(now);
             lastimage = now;
 
             // check if we are overrunning our max frame rate
             now = millis();
-            if(now > lastimage + msecPerFrame)
+            if(now > (lastimage + msecPerFrame))
                 printf("warning exceeding max frame rate of %d ms\n", now - lastimage);
         }
 
@@ -280,13 +313,12 @@ void loop()
         }
     }
     else {
-        client = rtspServer.accept();
+        rtsp_client = rtspServer.accept();
 
-        if(client) {
-            //streamer = new SimStreamer(&client, true);             // our streamer for UDP/TCP based RTP transport
-            streamer = new OV2640Streamer(&client, cam);             // our streamer for UDP/TCP based RTP transport
+        if(rtsp_client) {
+            streamer = new OV2640Streamer(&rtsp_client, cam);             // our streamer for UDP/TCP based RTP transport
 
-            session = new CRtspSession(&client, streamer); // our threads RTSP session and state
+            session = new CRtspSession(&rtsp_client, streamer); // our threads RTSP session and state
         }
     }
 #endif
